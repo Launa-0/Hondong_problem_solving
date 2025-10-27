4fc04236bdb920899be401b50165b73c3eb002cc
Launa-0 <136620815+Launa-0@users.noreply.github.com>
2025-10-27 16:42:31 +0900

    Update 백준_1515.py

--- 백준_1515.py
+++ 백준_1515.py
@@ -6,10 +6,10 @@ n = 0         # 현재 숫자(최종적으로 답)
 
 while idx < L:
     n += 1
-    for ch in str(n):
-        if idx < L and ch == T[idx]:
-            idx += 1
-            if idx == L:
+    for ch in str(n): #n을 문자열로 바꿔서 비교
+        if idx < L and ch == T[idx]: 
+            idx += 1 #현재 자릿수 ch가 배열 T의 문자와 같다면 매칭성공 ->  인덱스 증가시키기
+            if idx == L: #전부 맞추면 루프 탈출
                 break
 
 print(n)
